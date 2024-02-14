da135513e6634a5b0ff224d822fb06d1c3c38632
ebbam <99182071+ebbam@users.noreply.github.com>
2024-02-14 13:31:26 +0000

    Init code files

---
+++ code/dicts.R
@@ -0,0 +1,23 @@
+#### Variable labels for latex output ####
+
+setFixest_dict(c(employed = "Employed Persons"))
+
+# 
+# plot_dict = c("f(mines_diff,3)" = "-3", "f(mines_diff,2)" = "-2", "f(mines_diff, 1)" = "-1", 
+#               "mines_diff" = "0", "lag_diff" = "+1", "lag_diff2" = "+2", "lag_diff3" = "+3",
+#               `factor_type1:mines_diff` = "0",
+#               `factor_type2:mines_diff` = "0",
+#               `factor_type3:mines_diff` = "0",
+#               `factor_type1:lag_diff` = "+1",
+#               `factor_type2:lag_diff` = "+1",
+#               `factor_type3:lag_diff` = "+1",
+#               `factor_type1:lag_diff2` = "+2",
+#               `factor_type2:lag_diff2` = "+2",
+#               `factor_type3:lag_diff2` = "+2",
+#               `factor_type1:f(mines_diff, 1)` = "-1",
+#               `factor_type2:f(mines_diff, 1)` = "-1",
+#               `factor_type3:f(mines_diff, 1)` = "-1",
+#               `factor_type1:lag_diff3` = "+3",
+#               `factor_type2:lag_diff3` = "+3",
+#               `factor_type3:lag_diff3` = "+3"
+#               )
---
+++ code/useful_functions.R
@@ -0,0 +1,70 @@
+##############################################################
+library(tidycensus)
+get_state <- function(st){
+  fips_codes %>% filter(state_code == st) %>% pull(state) %>% unique %>% return(.)
+}
+
+get_code <- function(st_abb){
+  fips_codes %>% filter(state == st_abb) %>% pull(state_code) %>% unique %>% return(.)
+}
+
+#vec_get_state = Vectorize(get_state)
+
+## Function to identify FIPS code
+
+fips_format <- function(fipscode){
+  if (is.na(fipscode)) return("00000")
+  fips_c = as.character(fipscode)
+  test_chars = nchar(fips_c)
+  if (is.null(fipscode) | is.na(fipscode)) {
+    return (nchar(trunc(fipscode)))
+  }
+  else if (test_chars < 4) {
+    return (fips_c)
+  }
+  else if (test_chars < 5) {
+    return (paste0(0, fips_c, sep = ""))
+  }
+  else {
+    return (fips_c)
+  }
+}
+
+
+create_cols <- function(model){
+  pval <- model %>% pvalue() %>% 
+    lapply(., function(z) ifelse(z <= 0.001, 4,
+                                 ifelse(z > 0.001 & z <= 0.01, 3, 
+                                        ifelse(z > 0.01 & z <= 0.05, 2, 
+                                               ifelse(z > 0.05 & z <= 0.1, 1,0)))))
+  pval <- as.matrix(as.numeric(pval[1:3]))
+  iden <- matrix(nrow = 3, ncol = 3, data = 0)
+  diag(iden) <- unlist(lapply(coef(model)[1:3], sign))
+  return(-1*(t(pval) %*% iden))
+}
+
+
+df_va <- function(df){
+  fips_pre <- as.list(unique(df$fips))
+  for(a in 1:length(fips_pre)){
+    f = fips_pre[a]
+    if(f %in% getfips){
+      newfips = names(getfips[which(getfips == f)])
+      if(length(newfips) != 0){
+        for(b in 1:length(newfips)){
+          copy <- subset(df, fips == f)
+          copy$fips <- newfips[b]
+          df <- rbind(df, copy)
+        }
+      }else{
+      }
+      df <- subset(df, fips != f)
+    }
+  }
+  return(df)
+}
+
+
+
+# Initialise function for specific size of matrix
+c_mat <- function(var) {matrix(var, 18, 3072)}
