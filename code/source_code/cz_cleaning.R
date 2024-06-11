e1a4738d4a2dea323064b939bc49cb54f4e46144
ebbam <99182071+ebbam@users.noreply.github.com>
2024-06-11 11:11:25 +0100

    created source code folder with funs, cleaning, and scoping files

---
+++ code/source_code/cz_cleaning.R
@@ -0,0 +1,22 @@
+### Commuting Zones Cleaning
+
+# Official source of Commuting Zone codes - fips crosswalk (USDA ERS): https://www.ers.usda.gov/data-products/commuting-zones-and-labor-market-areas/
+# See also discussion here: https://osf.io/8vsh2
+# PSU keep record of comparable US labour market geographies: https://sites.psu.edu/psucz/
+# David Dorn's website with references and crosswalks: https://www.ddorn.net/data.htm
+
+
+# Current county-level dataset
+mines_restr <- readRDS(here('data/temp/mines_restr.RDS'))
+
+# Commuting zones
+czs <- read_xls(here('data/out/cz00_eqv_v1.xls')) %>%
+  select(FIPS, `Commuting Zone ID, 2000`, `Commuting Zone Population 2000`) %>% 
+  rename(fips = 1, 
+         cz_id= 2,
+         cz_population = 3) %>% 
+  mutate(fips = ifelse(fips == "46113", "46102", fips)) 
+
+
+# Testing whether all fips are present in commuting zones data - confirmed!
+czs %>% pull(fips) %>% setdiff(unique(mines_restr$fips), .) %>% length == 0
