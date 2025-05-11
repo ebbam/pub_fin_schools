cb69c8f79761779ebe5f1a58e4e35fd36c9c0d2e
ebbam <99182071+ebbam@users.noreply.github.com>
2025-05-11 11:25:44 +0100

    cz_cleaning includes fips conversion for map.

--- code/source_code/cz_cleaning.R
+++ code/source_code/cz_cleaning.R
@@ -21,4 +21,33 @@ czs <- read_xls(here('data/out/cz00_eqv_v1.xls')) %>%
   mutate(fips = ifelse(fips == "46113", "46102", fips),
          cz_id = as.character(cz_id))
 
+# CZs with full years
+gen_czs_years <- function(df, cz_conversion){
+  min_year <- min(df$year) 
+  max_year <- max(df$year)
+  czs_years <- czs %>% 
+    mutate(year = min_year) %>% 
+    select(fips, cz_id, year) %>% 
+    complete(fips, year = min_year:max_year) %>% 
+    group_by(fips) %>% 
+    fill(cz_id, .direction = "updown") %>% 
+    ungroup %>% 
+    select(cz_id, fips, year) 
+  return(czs_years)
+}
 
+# Function that converts commuting zones to fips codes for plotting via usmap
+cz_to_map <- function(df, czs_years, vars = NULL){
+  if(!is.null(vars)){
+    print("Returning dataframe with selected vars.")
+    temp <- mines_restr %>% 
+      select(cz_id, year, all_of(vars)) %>% 
+      left_join(czs_years, ., by = c("cz_id", "year")) 
+
+  }else{
+    print("Returning dataframe with all vars.")
+    temp <- czs_years %>% 
+      left_join(., mines_restr, by = c("cz_id", "year"))
+  }
+  return(temp)
+}
